31a874cddf7ed608aa828e20db2edb9fd5d9708f
Lazoroz <80781@glr.nl>
2022-04-23 21:37:38 +0200

    failswitch added

--- js/chat.js
+++ js/chat.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
     //console.log('dsdsd');
 
-    $( "form" ).submit(function (e) { 
+    $("form").submit(function (e) {
         e.preventDefault();
 
         var message = $('#message').val();
-        
-        $.post("../php/add.php",{message, id:"<?php echo $token;?>" } )  
-        .done(function(message){
-            alert('toegevoegd!');
-        });
-    }); 
-     
-    
+
+        $.post("../php/add.php", { message, id: "<?php echo $token;?>" })
+            .done(function (message) {
+                alert('toegevoegd!');
+            })
+            .fail(function(){
+                alert('erroe');
+            });
+
+    });
+
+
 });
